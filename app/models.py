1955f5c1412769bdacb43b3cf6a5cc7aced53140
unknown <DexterA@DexterA-L1.corp.intusurg.com>
2015-07-07 16:58:49 -0700

    '07/07'

--- app/models.py
+++ app/models.py
@@ -6,7 +6,18 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	
+        def __repr__(self):
+		return '<User %r>' % (self.nickname)
+class Post(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        body = db.Column(db.String(140))
+        timestamp=db.Column(db.DateTime)
+        #foreign key brings the link to user.id
+        user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+        
 	
 	#tells python how to print objects of this class
 	def __repr__(self):
-		return '<User %r>' % (self.nickname)
+		return '<Post %r>' % (self.body)
--- test/db_migrate.py
+++ test/db_migrate.py
@@ -1,28 +1,19 @@
 #!flask/bin/python
-#compares app.db to app/models
 import imp
 from migrate.versioning import api
 from app import db
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-#set the location for version 
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-#gets new version number
 migration = SQLALCHEMY_MIGRATE_REPO + ('/versions/%03d_migration.py' % (v+1))
-
 tmp_module = imp.new_module('old_model')
-#creates model?
 old_model = api.create_model(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-#executes the two modules
 exec(old_model, tmp_module.__dict__)
-
-script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI, 
-					SQLALCHEMY_MIGRATE_REPO,
-					tmp_module.meta, db.metadata)
-#opens the new version template then writes the update into it
+script = api.make_update_script_for_model(SQLALCHEMY_DATABASE_URI,
+                                          SQLALCHEMY_MIGRATE_REPO,
+                                          tmp_module.meta, db.metadata)
 open(migration, "wt").write(script)
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 print('New migration saved as ' + migration)
 print('Current database version: ' + str(v))
-
